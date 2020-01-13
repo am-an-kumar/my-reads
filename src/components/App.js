5fb430eedabc18c1fab0fb8f5fb5c8501c6dae43
Aman <amankumar.edith@gmail.com>
2020-01-13 20:06:34 +0530

    Added routes for / and /search

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,25 @@
 import React from 'react'
 import { hot } from 'react-hot-loader'
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from 'react-router-dom'
+import Header from './Header'
+import Home from './Home'
+import Search from './Search'
 
-const App = () => <h1>My Reads</h1>
+const App = () => (
+  <Router basename='/'>
+    <Header />
+    <Switch>
+      <Route exact path='/' component={Home} />
+      <Route path='/search' component={Search} />
+      <Redirect to='/' />
+    </Switch>
+  </Router>
+)
 
 // hot(module)(App) - enables hot reloading for component tree rooted at App
 export default hot(module)(App)
---
+++ src/components/Header.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const Header = () => <h1>My Reads</h1>
+
+export default Header
