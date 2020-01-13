0164195aac577b2cd843216f8d6031369aca53df
Aman <amankumar.edith@gmail.com>
2020-01-13 22:42:20 +0530

    Added button on home page to redirect to search page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from './Header'
 import BookStore from './BookStore'
+import AddBooksButton from './AddBooksButton'
 
 const Home = props => (
   <div className='list-books'>
     <Header />
     <BookStore {...props} />
+    <AddBooksButton />
   </div>
 )
 
