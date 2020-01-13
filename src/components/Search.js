88dfcc3cd0aeb4f72e8730ae74e7ed88d6375f71
Aman <amankumar.edith@gmail.com>
2020-01-13 22:51:55 +0530

    Added search page UI

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,5 +1,12 @@
 import React from 'react'
+import SearchForm from './SearchForm'
+import SearchResults from './SearchResults'
 
-const Search = () => <p>Search component</p>
+const Search = () => (
+  <div className='search-books'>
+    <SearchForm />
+    <SearchResults />
+  </div>
+)
 
 export default Search
---
+++ src/components/SearchForm.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const SearchForm = () => (
+  <div className='search-books-bar'>
+    <Link to='/'>
+      <button className='close-search'>Close</button>
+    </Link>
+    <div className='search-books-input-wrapper'>
+      <input type='text' placeholder='Search by title or author' />
+    </div>
+  </div>
+)
+
+export default SearchForm
