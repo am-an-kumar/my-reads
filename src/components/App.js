4e33b800edafd5b4b1d8e5a8fac8ea86104bba34
Aman <amankumar.edith@gmail.com>
2020-01-13 16:34:44 +0530

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import { hot } from 'react-hot-loader'
+
+const App = () => <h1>My Reads</h1>
+
+// hot(module)(App) - enables hot reloading for component tree rooted at App
+export default hot(module)(App)
