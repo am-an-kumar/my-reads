46ae765f1c957bb356f229a30fbf226500d9fce4
Aman <amankumar.edith@gmail.com>
2020-01-18 05:40:00 +0530

    Fixed body scroll issue with keyword suggestion dropdown being open

--- src/components/search/SearchSuggestions.js
+++ src/components/search/SearchSuggestions.js
@@ -1,21 +1,33 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
-const SearchSuggestions = ({ filteredKeywords, onSelectChangeHandler }) => (
-  <select
-    defaultValue=''
-    size={10}
-    className='keyword-dropdown'
-    onChange={event => onSelectChangeHandler(event.target.value)}
-  >
-    {filteredKeywords.map(keyword => (
-      <option key={keyword} value={keyword}>
-        {keyword}
-      </option>
-    ))}
-  </select>
-)
+class SearchSuggestions extends Component {
+  componentDidMount() {
+    document.body.style.overflowY = 'hidden'
+  }
 
+  componentWillMount() {
+    document.body.style.overflowY = 'unset'
+  }
+
+  render() {
+    const { filteredKeywords, onSelectChangeHandler } = this.props
+    return (
+      <select
+        defaultValue=''
+        size={10}
+        className='keyword-dropdown'
+        onChange={event => onSelectChangeHandler(event.target.value)}
+      >
+        {filteredKeywords.map(keyword => (
+          <option key={keyword} value={keyword}>
+            {keyword}
+          </option>
+        ))}
+      </select>
+    )
+  }
+}
 SearchSuggestions.propTypes = {
   filteredKeywords: PropTypes.array.isRequired,
   onSelectChangeHandler: PropTypes.func.isRequired,
